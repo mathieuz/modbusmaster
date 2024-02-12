596659eefd372550f548cb2cc059714ef46a09a0
mathieuz <matheusandrewww@gmail.com>
2024-02-12 09:42:34 -0300

    Adicionado: dir testes + populandoArrayDeHex

--- ModbusMaster.h
+++ ModbusMaster.h
@@ -26,13 +26,19 @@ public:
     }
     */
 
-    int* testeRetornoArray(uint size/*, uint8_t deviceAddress, uint8_t registerAddressH, uint8_t registerAddressL, uint8_t lengthL, uint8_t lengthH*/) {
-        int* arr = new int[size];
+    uint8_t* testeRetornoArray(uint size/*, uint8_t deviceAddress, uint8_t registerAddressH, uint8_t registerAddressL, uint8_t lengthL, uint8_t lengthH*/) {
+        uint8_t* arr = new uint8_t[size];
 
-        arr[0] = 10;
-        arr[1] = 20;
-        arr[2] = 30;
-        arr[3] = 40;
+        /*
+        arr[0] = 0x01;
+        arr[1] = 0x02;
+        arr[2] = 0x03;
+        arr[3] = 0x04;
+        */
+
+        for (uint i = 0; i < size; i++) {
+            arr[i] = (0x00 << 4) + i;
+        }
 
         return arr;
     }
