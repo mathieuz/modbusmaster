9f76546398065a275ea6f70db1921d065bd2fec8
mathieuz <matheusandrewww@gmail.com>
2024-02-16 11:23:55 -0300

    .

--- modbusmaster.ino
+++ modbusmaster.ino
@@ -4,12 +4,12 @@
 ModbusMaster ms = ModbusMaster(20000);
 
 //Estrutura de Requisição.
-uint8_t deviceAddress    = 0x11; //Endereço do escravo.
+uint8_t deviceAddress    = 0x02; //Endereço do escravo.
 uint8_t functionCode     = 0x01; //Código da função.
 uint8_t startAddressHigh = 0x00; //Byte do endereço inicial.
-uint8_t startAddressLow  = 0x13; //Byte do endereço inicial.
+uint8_t startAddressLow  = 0x20; //Byte do endereço inicial.
 uint8_t lengthHigh       = 0x00; //Byte da quantidade de endereços a serem lidos (a partir do endereço inicial).
-uint8_t lengthLow        = 0x25; //Byte da quantidade de endereços a serem lidos (a partir do endereço inicial).
+uint8_t lengthLow        = 0x0C; //Byte da quantidade de endereços a serem lidos (a partir do endereço inicial).
 
 void setup() {
     delay(3000);
